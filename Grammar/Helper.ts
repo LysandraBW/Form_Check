de6d782f4a7fe6f0446ee275febabe8d38a0cf12
Lysandra Belnavis-Walters <lbelnaviswalters@gmail.com>
2024-05-04 00:39:48 -0400

    Obsessing over small details

--- Grammar/Helper.ts
+++ Grammar/Helper.ts
@@ -1,12 +1,8 @@
 export function toList(w: string[], conjunction: string) {
     const wLength = w.length;
-    
     switch (wLength) {
-        case 1: 
-            return w[0];
-        case 2: 
-            return `${w[0]} ${conjunction} ${w[1]}`;
-        default: 
-            return `${w.slice(0, wLength - 1).join(", ")}, ${conjunction} ${w[wLength - 1]}`;
+        case 1: return w[0];
+        case 2: return `${w[0]} ${conjunction} ${w[1]}`;
+        default: return `${w.slice(0, wLength - 1).join(", ")}, ${conjunction} ${w[wLength - 1]}`;
     }
 }
